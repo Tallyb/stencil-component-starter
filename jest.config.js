6294549c0d95e0eeeacd7b59266c2c9cbf0fb294
tallyb <tally.barak@gmail.com>
2018-11-29 13:29:35 +0200

    install pupppeteer

---
+++ jest.config.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const { defaults: tsjPreset } = require('ts-jest/presets');
+
+const config = {
+    transform: {
+        
+    },
+    transform: {
+        ...tsjPreset.transform
+     // '^.+\\.(ts|tsx)$': '<rootDir>/node_modules/@stencil/core/testing/jest.preprocessor.js'
+    },
+    moduleFileExtensions: [
+      'ts',
+      'tsx',
+      'js',
+      'json',
+      'jsx'
+    ],
+    testMatch: [
+      '**/__tests__/**/*.ts?(x)',
+      '**/?(*.)+(spec|test).ts?(x)'
+    ],
+    testPathIgnorePatterns: [
+      '/dist/',
+      '/node_modules/',
+      '/www/',
+      '/bit/'
+    ]
+};
+module.exports=config;
